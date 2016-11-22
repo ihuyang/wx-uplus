ef8c99e857d5ed94faf30f2d150f8ef168f7a30a
Calarence <749826557@qq.com>
2016-11-22 22:18:45 +0800

    增加反馈提示和网络状态提示

--- pages/lists/lists.js
+++ pages/lists/lists.js
@@ -3,6 +3,9 @@ Page({
   data: {
     lastid: 0,
     newsList: [],
+    toastHidden: true,
+    modalHidden: true,
+    isfirst: 1
 
   },
   //事件处理函数
@@ -23,12 +26,19 @@ Page({
       },
       success: function (res) {
         console.log(res.data)
+        if (res.data.results.length == 0) {
+          that.setData({
+            toastHidden: false
+          })
+          return false
+        }
         var len = res.data.results.length
         that.setData({
-          lastid: res.data.results[len-2].id
+          lastid: res.data.results[len - 2].id
         })
         var dataArr = that.data.newsList
         var newData = dataArr.concat(res.data.results)
+        console.log(res.data.next)
         that.setData({
           newsList: newData
         })
@@ -39,12 +49,41 @@ Page({
     console.log('onLoad')
     var that = this
     this.loadData(0)
+    this.getNetwork()
 
   },
   loadmore: function (event) {
-     var id = event.currentTarget.dataset.lastid
+    var id = event.currentTarget.dataset.lastid
+    var isfirst = event.currentTarget.dataset.isfirst
+    console.log(isfirst)
+    if (this.getNetwork != "wifi" && isfirst == 1) {
+      this.setData({
+        modalHidden: false,
+        isfirst: 0
+      })
+   
+    }
+    
     console.log("ddddddddd" + id)
     this.loadData(id)
+  },
+  toastChange: function () {
+    this.setData({
+      toastHidden: true
+    })
   }
-
+  ,
+  getNetwork: function () {
+    wx.getNetworkType({
+      success: function (res) {
+        var networkType = res.networkType // 返回网络类型2g，3g，4g，wifi
+        return networkType
+      }
+    })
+  },
+  modalChange: function () {
+    this.setData({
+      modalHidden: true
+    })
+  }
 })
